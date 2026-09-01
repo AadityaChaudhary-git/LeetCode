f607ec4566471054f5ec27520e8fc9167daee554
Aaditya Chaudhary <aditya.879161@gmail.com>
2026-09-01 20:00:53 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.51%) - LeetHub

---
+++ Python/0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def sortColors(self,nums ):
+        """
+        :type : List[int]
+        :rtype: None Do not return anything, modify  in-place instead.
+        """
+        start =0
+        mid =0
+        end=len(nums)-1
+
+        while mid <= end:
+             if nums[mid]==2:
+                nums[mid],nums[end]=nums[end],nums[mid]
+                end-=1
+             elif nums[mid]==1:
+                mid+=1
+             elif nums[mid]==0 :
+                nums[start],nums[mid]=nums[mid],nums[start]
+                start+=1
+                mid+=1
+
+
+
+
+
+            
+              
+             
+
+
+        
+
+        
+
+        
+
+
+
+
+
+
